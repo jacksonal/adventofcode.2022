d6b33362201cb4fd2d459c6e62cf9137105d2e42
Alex Jackson <alex.lee.jackson@gmail.com>
2022-12-12 19:55:45 -0500

    day 11

--- 2022_11.py
+++ 2022_11.py
@@ -25,50 +25,44 @@ class Monkey:
     self.targetMonkeys = targets
     self.inspectCount = 0
 
-monkeyMap = dict()
-for m in getFileContents('./test_input.txt').split('\n\n'):
-  lines = m.split('\n')
-  label = int(lines[0][-2])
-  items = [int(i) for i in lines[1].split(':')[1].split(',')]
-  worryop = eval('lambda old:' + lines[2].split('=')[1])
-  divBy = int(lines[3].split()[-1])
-  targets = (int(lines[4][-1]), int(lines[5][-1]))
-  
-  monkeyMap[label] = Monkey(items,worryop, divBy, targets)
+def buildMonkeyMap() -> dict:
+  map = dict()
+  for m in getFileContents('./input.txt').split('\n\n'):
+    print(m)
+    lines = m.split('\n')
+    label = int(lines[0][-2])
+    items = [int(i) for i in lines[1].split(':')[1].split(',')]
+    worryop = eval('lambda old:' + lines[2].split('=')[1])
+    divBy = int(lines[3].split()[-1])
+    targets = (int(lines[4][-1]), int(lines[5][-1]))
+    
+    map[label] = Monkey(items,worryop, divBy, targets)
+  return map
 
-for round in range(20):
-  print(f'ROUND {round}')
+monkeyMap = buildMonkeyMap()
+for round in range(10000):
+  #print(f'ROUND {round}')
   for id in range(len(monkeyMap)):
     monkey = monkeyMap[id]
     # inspect, update worry, throw each item
-    print(f'monkey {id}')
+    #print(f'monkey {id}')
     while(len(monkey.items) > 0):
       current = monkey.items.pop(0)
-      print(f'\tinspecting item {current}')
+      #print(f'\tinspecting item {current}')
       current = monkey.updateWorry(current)
-      print(f'\t\tupdating item to {current}')
+      #print(f'\t\tupdating item to {current}')
       #monkey gets bored, decrease worry level only in part 1
-      current = current // 3
-      print(f'decrease worry to {current}')
+      #current = current // 3
+      #print(f'decrease worry to {current}')
       monkey.inspectCount += 1
+      current = current % 9699690 #must mod the value by LCM of all the divisors. why? to make sure it doesn't affect the divisibility by the other test values.
       if current % monkey.worryDivisibleTest == 0:
-        print(f'\t\t{current} is divisible by {monkey.worryDivisibleTest}. pass to {monkey.targetMonkeys[0]}')
-        # part 2 very large worry scores. but I only care if the value is divisible
-        # set the worry score to modulo next monkey's divisor?
-        #print(f'\t\tset {current} to mod {monkeyMap[monkey.targetMonkeys[0]].worryDivisibleTest}')
-        #current = current % monkeyMap[monkey.targetMonkeys[0]].worryDivisibleTest
+        #print(f'\t\t{current} is divisible by {monkey.worryDivisibleTest}. pass to {monkey.targetMonkeys[0]}')
         monkeyMap[monkey.targetMonkeys[0]].items.append(current)
       else:
-        print(f'\t\t{current} is not divisible by {monkey.worryDivisibleTest}. pass to {monkey.targetMonkeys[1]}')
-        #print(f'\t\tset {current} to mod {monkeyMap[monkey.targetMonkeys[1]].worryDivisibleTest}')
-        #current = current % monkeyMap[monkey.targetMonkeys[1]].worryDivisibleTest
+        #print(f'\t\t{current} is not divisible by {monkey.worryDivisibleTest}. pass to {monkey.targetMonkeys[1]}')
         monkeyMap[monkey.targetMonkeys[1]].items.append(current)
-  # print monkey inventories
-  if round % 1000 == 0 or round == 19:
-    print(f'after round {round}')
-    for id in range(len(monkeyMap)):
-      m = monkeyMap[id]
-      print(f'monkey {id}:', m.inspectCount)
+
 inspectScores = sorted([m.inspectCount for m in monkeyMap.values()])
 print(inspectScores)
 print(inspectScores[-1] * inspectScores[-2])
